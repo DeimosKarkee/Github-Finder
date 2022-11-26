110da8cfb59374452157736930342617cbb8eac6
Deimos Karkee <deimoskarrkey@outlook.com>
2022-11-26 11:09:59 +1100

    Created pages and added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <div>
-      <h1>Github Finder Test</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/*" element={<NotFound />}></Route>
+        <Route path="/notfound" element={<NotFound />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
